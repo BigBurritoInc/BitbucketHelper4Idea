4e7d640ad355d650d85bd0dba258a2ec71561c1e
Dmitrii Paliuga <dmpalyuga@devexperts.com>
2018-08-05 18:42:18 +0300

    Trying to call git4idea (with no success)

--- src/Git.kt
+++ src/Git.kt
@@ -1,5 +1,56 @@
-class Git: VCS {
+import com.intellij.ide.DataManager
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.project.Project
+import com.intellij.vcsUtil.VcsUtil
+import git4idea.GitVcs
+import git4idea.branch.GitBranchUtil
+import git4idea.branch.GitBrancher
+import git4idea.repo.GitRepository
+
+
+//todo: get rid of null-checks
+object Git: VCS {
+
     override fun checkoutBranch(branch: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        println("Checking out $branch")
+        val currentProject = currentProject()
+        val currentRepository = currentRepository()
+        if (currentProject != null && currentRepository != null) {
+            val brancher = GitBrancher.getInstance(currentProject)
+            brancher.checkout(branch, false, listOf(currentRepository), null as Runnable?)
+        } else {
+            println("prj or repo is null $currentProject $currentRepository")
+        }
+    }
+
+    override fun currentBranch(): String {
+        val repository = currentRepository()
+        if (repository != null)
+            return GitBranchUtil.getDisplayableBranchText(repository)
+        return ""
+    }
+
+    fun currentProject(): Project? {
+        return CommonDataKeys.PROJECT.getData(DataManager.getInstance().dataContext)
+    }
+
+    fun currentRepository(): GitRepository? {
+        val prj = currentProject()
+        if (prj != null && gitVcs() != null)
+            return GitBranchUtil.getCurrentRepository(prj)
+        return null
+    }
+
+    fun gitVcs(): GitVcs? {
+        val prj = currentProject()
+        val baseDir = prj?.baseDir
+        if (prj != null && baseDir != null) {
+            val vcs = VcsUtil.getVcsFor(prj, baseDir)
+            //todo: vcs has class GitVcs, although it is a different GitVcs class that we expected.
+            //2 different plugins' classloaders might loaded GitVsc two times???
+            if (vcs is GitVcs)
+                return vcs
+        }
+        return null
     }
 }
--- src/VCS.kt
+++ src/VCS.kt
@@ -1,3 +1,4 @@
 interface VCS {
     fun checkoutBranch(branch: String)
+    fun currentBranch(): String
 }
