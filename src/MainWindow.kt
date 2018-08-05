8c32d3b6ab9095bca82dd82ad63d78948f4a5105
Dmitrii Paliuga <dmpalyuga@devexperts.com>
2018-08-05 15:36:38 +0300

    Plugin window structure

--- src/MainWindow.kt
+++ src/MainWindow.kt
@@ -1,9 +1,20 @@
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.components.JBLabel
+import com.intellij.ui.content.ContentManager
+import javax.swing.JComponent
 
 class MainWindow : ToolWindowFactory {
     override fun createToolWindowContent(prj: Project, window: ToolWindow) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val cm = window.contentManager
+        //todo: add some real content here
+        addTab(cm, JBLabel("Reviewing"), "Reviewing")
+        addTab(cm, JBLabel("Created"), "Created")
+    }
+
+    private fun addTab(contentManager: ContentManager, component: JComponent, tabName: String) {
+        val content = contentManager.factory.createContent(component, tabName, false)
+        contentManager.addContent(content)
     }
 }
