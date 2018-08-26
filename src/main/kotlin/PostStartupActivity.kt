ff35de816d3adfe6391a54cb48db21cf1cf98def
Dmitrii Kopylov <dkopylov@devexperts.com>
2018-08-26 15:35:21 +0300

    #1 Make repo/user/authentication parameters configurable

--- src/main/kotlin/PostStartupActivity.kt
+++ src/main/kotlin/PostStartupActivity.kt
@@ -1,19 +1,28 @@
 import bitbucket.BitbucketClient
 import bitbucket.createClient
 import bitbucket.data.PagedResponse
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 import com.intellij.util.concurrency.AppExecutorUtil
 import rx.Observable
 import ui.Model
+import java.util.concurrent.ScheduledFuture
 import java.util.concurrent.TimeUnit
 import java.util.function.Consumer
 
-class PostStartupActivity : StartupActivity {
-    override fun runActivity(project: Project) {
-        AppExecutorUtil.getAppScheduledExecutorService().scheduleWithFixedDelay(
-                UpdateTask(createClient()), 0, 15, TimeUnit.SECONDS)
+object PostStartupActivity : StartupActivity {
+    var future:ScheduledFuture<*>? = null // todo get rid of null, find more right way to store
+    var password:CharArray? = null
+
+    override fun runActivity(project: Project) { // todo we have project here, we must use it
+        reschedule()
+    }
+
+    fun reschedule() {
+        if (future != null)
+            (future as ScheduledFuture<*>).cancel(true)
+        future = AppExecutorUtil.getAppScheduledExecutorService().scheduleWithFixedDelay(
+                UpdateTask(createClient()), 0, 15, TimeUnit.SECONDS) as ScheduledFuture<UpdateTask>
     }
 
     class UpdateTask(private val client: BitbucketClient): Runnable {
--- src/main/kotlin/bitbucket/Factory.kt
+++ src/main/kotlin/bitbucket/Factory.kt
@@ -2,21 +2,32 @@ package bitbucket
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.intellij.ide.DataManager
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.components.ServiceManager
+import com.intellij.openapi.ui.DialogWrapper
 import http.HttpAuthRequestFactory
 import org.apache.http.impl.nio.client.HttpAsyncClients
 import org.apache.http.nio.client.HttpAsyncClient
+import ui.Storer
 import java.net.URL
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.util.*
+import javax.swing.JComponent
+import javax.swing.JPasswordField
 
 fun createClient(): BitbucketClient {
-    val info = bitbucketInfo()
+    val info = BitbucketInfo()
+
+    if (PostStartupActivity.password == null) {
+        val dialog = PasswordPromptDialog(false)
+        dialog.showAndGet()
+        PostStartupActivity.password = dialog.getPassword()
+    }
+
     val objectMapper = ObjectMapper()
     objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
     return BitbucketClient(
             createHttpClient(),
-            HttpAuthRequestFactory(info.user, info.password),
+            HttpAuthRequestFactory(info.user, String(PostStartupActivity.password!!)),
             info.baseURL, info.project, info.repoSlug, info.user,
             objectMapper.reader(), objectMapper.writer())
 }
@@ -27,22 +38,25 @@ private fun createHttpClient(): HttpAsyncClient {
     return client
 }
 
-private fun loadProperties(): Properties {
-    val br = Files.newBufferedReader(Paths.get("C:\\opt\\repo.properties"))
-    val props = Properties()
-    props.load(br)
-    br.close()
-    return props
+class BitbucketInfo {
+    val storer = ServiceManager.getService<Storer>(CommonDataKeys.PROJECT.getData(DataManager.getInstance().getDataContext())!!, Storer::class.java);
+    val baseURL: URL = URL(storer.settings.url)
+    val project: String = storer.settings.project
+    val repoSlug: String = storer.settings.slug
+    val user: String = storer.settings.login
 }
 
-private fun bitbucketInfo(): BitbucketInfo {
-    return BitbucketInfo(loadProperties())
-}
+class PasswordPromptDialog(canBeParent: Boolean) : DialogWrapper(canBeParent) {
+    val passwordField = JPasswordField()
+    init {
+        title = "MyBitbucket"
+        init()
+    }
+    override fun createCenterPanel(): JComponent? {
+        return passwordField
+    }
 
-class BitbucketInfo(props: Properties) {
-    val baseURL: URL = URL(props.getProperty("url"))
-    val project: String = props.getProperty("project")
-    val repoSlug: String = props.getProperty("slug")
-    val user: String = props.getProperty("user")
-    val password: String = props.getProperty("pass")
+    fun getPassword(): CharArray {
+        return passwordField.password
+    }
 }
