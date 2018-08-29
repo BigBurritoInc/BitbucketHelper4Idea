0c1ca7713b15ca994dc1ffc2e3e773f55bc41200
Nikita Podshivalov <npodshivalov@devexperts.com>
2018-08-29 19:31:05 +0300

    Provides links to PRs Related issue: https://github.com/palyuga/BitbucketHelper4Idea/issues/6

--- src/main/kotlin/bitbucket/data/User.kt
+++ src/main/kotlin/bitbucket/data/User.kt
@@ -3,8 +3,7 @@ package bitbucket.data
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class User(@JsonProperty("name") val name: String,
-           @JsonProperty("emailAddress") val emailAddress: String,
-           @JsonProperty("id") val id: Long,
-           @JsonProperty("displayName") val displayName: String) {
-
-}
+                @JsonProperty("emailAddress") val emailAddress: String,
+                @JsonProperty("id") val id: Long,
+                @JsonProperty("displayName") val displayName: String,
+                @JsonProperty("links") val links: Links)
