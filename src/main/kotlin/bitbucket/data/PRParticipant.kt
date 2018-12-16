7f7806e9959e9cd25e7168eaebe3afe34a673ee3
Aleksandr Krasilnikov <akrasilnikov@devexperts.com>
2018-12-16 22:52:25 +0300

    Add support for NEEDS WORK status

--- src/main/kotlin/bitbucket/data/PRParticipant.kt
+++ src/main/kotlin/bitbucket/data/PRParticipant.kt
@@ -1,7 +1,10 @@
 package bitbucket.data
 
+import bitbucket.httpparams.ParticipantStatus
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class PRParticipant(
         @JsonProperty("user") val user: User,
-        @JsonProperty("approved") val approved: Boolean)
+        @JsonProperty("approved") val approved: Boolean,
+        @JsonProperty("status") val status: ParticipantStatus
+)
---
+++ src/main/kotlin/ui/ParticipantStatusColors.kt
@@ -0,0 +1,21 @@
+package ui
+
+import bitbucket.httpparams.ParticipantStatus
+import java.awt.Color
+
+/**
+ * Colors configuration for different [ParticipantStatus]es
+ */
+object ParticipantStatusColors {
+    private val TRANSPARENT = Color(0, 0, 0, 0)
+
+    private val colors: Map<ParticipantStatus, Color> = mapOf(
+            ParticipantStatus.UNAPPROVED to TRANSPARENT,
+            ParticipantStatus.APPROVED to Color(89, 168, 105),
+            ParticipantStatus.NEEDS_WORK to Color(255, 153, 51)
+    )
+
+    fun getColors(status: ParticipantStatus): Color {
+        return colors.getOrDefault(status, TRANSPARENT)
+    }
+}
