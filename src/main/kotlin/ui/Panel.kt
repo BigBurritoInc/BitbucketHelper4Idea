6fb3eb1e25cb1b8265699c254ed029ed5cfb0db7
Dmitrii Paliuga <dmpalyuga@devexperts.com>
2019-01-25 15:45:29 +0300

    Fixing vertcal align of pull-requests in the panel

--- src/main/kotlin/ui/Panel.kt
+++ src/main/kotlin/ui/Panel.kt
@@ -2,29 +2,21 @@ package ui
 
 import bitbucket.data.PR
 import java.awt.Component
-import java.awt.GridBagLayout
 import javax.swing.JPanel
-import java.awt.GridBagConstraints
-import java.awt.Insets
+import javax.swing.BoxLayout
+
+
 
 
 abstract class Panel : JPanel(), Listener {
 
-    private val gbc: GridBagConstraints = GridBagConstraints()
-
     init {
-        val layout = GridBagLayout()
-        gbc.fill = GridBagConstraints.HORIZONTAL
-        gbc.weightx = 1.0
-        gbc.gridx = 0
-        gbc.anchor = GridBagConstraints.WEST
-        gbc.insets = Insets(3, 3, 3, 3)
-        setLayout(layout)
+        layout = BoxLayout(this, BoxLayout.Y_AXIS)
     }
 
     fun dataUpdated(diff: Diff) {
         diff.added.values.sortedBy { it.updatedAt }
-                .forEach{ add(createPRComponent(it), gbc, 0) }
+                .forEach{ add(createPRComponent(it), 0) }
 
         val toRemove = mutableListOf<Component>()
         for (i in 0 until componentCount) {
